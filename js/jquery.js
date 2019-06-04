783ba28d76fe46c492e57fe2dd86a82814f0df01
Isaias140999 <51375738+Isaias140999@users.noreply.github.com>
2019-06-04 10:44:56 -0500

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+function enviar() {
+	
+	var nombre = $("nombre").val();
+    var email = $("email").val();
+    var comentario = $("comentario").val();
+	
+	
+    alert("¡Mensaje enviado, gracias por escribirnos, tu opinion es muy importante para nosotros!");
+ }
---
+++ trabajo/mayor.js
@@ -0,0 +1,12 @@
+function mayor(){
+var x=parseInt(document.getElementById("f2").value);
+
+if(x>=2001){
+
+alert('es menor de edad');
+}else{
+	
+	alert('gracias´por su registro');
+}
+
+}
